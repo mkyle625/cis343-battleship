03bb438eba1588cf5eef15e576ad3262f548903f
boermacon <boermacon@gmail.com>
2022-03-13 15:34:08 -0400

    Commented StudentGame

--- BattleshipCPP/BattleshipCPP/StudentGame.cpp
+++ BattleshipCPP/BattleshipCPP/StudentGame.cpp
@@ -22,15 +22,9 @@ Game::Game() {
 	ships.insert(ships.begin() + 3, *submarine);
 	ships.insert(ships.begin() + 4, *patrolBoat);
 
-	// Ship temp = ships.at(3);
-	// std::cout << temp.getName() << std::endl;
-
 	// Create new boards for both the player and the computer
 	player = *(new Board());
 	computer = *(new Board());
-
-	//Board* playerBoard = new Board();
-	
 }
 
 /**
@@ -45,7 +39,7 @@ void Game::beginGame() {
 
 	// Introduction
 	std::cout << "Welcome to Battleship!" << std::endl;
-	std::cout << "You will be facing off against the legendary CAPTAIN <name>" << std::endl;
+	std::cout << "You will be facing off against the legendary CAPTAIN HENRY MORGAN" << std::endl;
 
 	std::cout << "To get started, place your ships by following the prompts." << std::endl;
 
@@ -100,13 +94,10 @@ void Game::placeShips() {
  * Handle the computer placing ships.
  */
 void Game::placeShipsPC() {
-	// Place ship logic here
-	// Probably want to loop through every ship and have randomly place it, checking if its valid
-
-	// Gonna use the same logic as above, except choose random numbers and see if they work
 	// Loop through every ship
 	for (Ship ship : ships)
 	{
+		//Setup temp vars
 		bool shipPlaced = false;
 		int x, y, tempD = 0;
 		int min = 0;
@@ -130,14 +121,10 @@ void Game::placeShipsPC() {
 			else
 				d = HORIZONTAL;
 
+			//Place and check that the ship was placed
 			if (place(x, y, d, ship, computer)) {
 				shipPlaced = true;
-				//std::cout << "Ship is being placed at " << x << " , " << y << std::endl;
-				//std::cout << "COMPUTER BOARD: " << std::endl;
-				//std::cout << computer << std::endl;
 			}
-				//std::cout << "Ship cannot be placed here. Try again." << std::endl;
-
 		}
 	}
 }
@@ -181,6 +168,7 @@ bool Game::place(const int& x, const int& y, Direction d, const Ship& s, Board& 
 		}
 
 	}
+	//Check Vertical
 	if (d == VERTICAL) {
 		bool isEmpty = true;
 		for (size_t i = 0; i < s.getSpaces(); i++)
@@ -209,20 +197,24 @@ bool Game::place(const int& x, const int& y, Direction d, const Ship& s, Board& 
  * Call human turn/computer turn until someone wins.
  */
 void Game::run() {
-
+	//While neither player has the number of hits required to win.
 	while((player.count() != 17) && (computer.count() != 17)){
+		//Run turns
 		humanTurn();
 		computerTurn();
+		//Print boards after hits are made
 		std::cout << "PLAYER BOARD: " << std::endl;
 		std::cout << player << std::endl;
 		std::cout << "COMPUTER BOARD: " << std::endl;
 		std::cout << computer << std::endl;
+		//Print who is winning
 		if(computer.operator<(player)){
 			std::cout << "PLAYER IS WINNING" << std::endl;
 		} else {
 			std::cout << "COMPUTER IS WINNING" << std::endl;
 		}
 	}
+	//Print who has won
 	if(computer.operator<(player)){
 		std::cout << "PLAYER WON" << std::endl;
 	} else {
@@ -230,24 +222,32 @@ void Game::run() {
 	}
 }
 
+/**
+ * @brief Method loop for the human player to fire shots in their turn
+ * 
+ */
 void Game::humanTurn() {
-
-	// Process turn
+	//temp vars
 	int x = 0;
 	int y = 0;
-	//std::cout << "Where do you wish to place " << ship << "?" << std::endl;
+	//Ask and read where to fire
 	std::cout << "Where do you wish to fire? (X cord then Y cord)" << std::endl;
 	std::cin >> x;
 	std::cin >> y;
+	//Check that coordinates are valid
 	if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT){
 		std::cout << "Invalid coordinates, please try again." << std::endl;
 		humanTurn();
+	//Cords are valid
 	} else {
+		//Check for miss
 		if(computer[x][y] == EMPTY){
 			computer[x][y] = MISS;
 			std::cout << "Player MISS!" << std::endl;
+		//Check for a repeat miss
 		} else if (computer[x][y] == MISS){
 			std::cout << "Player MISSES Again!" << std::endl;
+		//Check for hit
 		} else {
 			computer[x][y] = HIT;
 			std::cout << "Player HIT!" << std::endl;
@@ -255,27 +255,36 @@ void Game::humanTurn() {
 	}
 }
 
+/**
+ * @brief Method loop for the computer player to fire shots in their turn. AI scans through every single space in the board.
+ * 
+ */
 void Game::computerTurn() {
-
-	// Same as human turn, but add random elements 
+	//Temp vars preset for invalid cords
 	int x = -10;
 	int y = -10;
+	//Iterate through the board
 	for (size_t i = 0; i < WIDTH; i++)
 	{
-		// Rows
 		for (size_t j = 0; j < HEIGHT; j++)
 		{
+			//Find a space that hasn't been shot at yet
 			if ((player[i][j] != MISS) && (player[i][j] != HIT)){
+				//load it into the temp vars
 				x = i;
 				y = j;
 			}
+			//Break loop if the temp vars are filled
 			if ((x != -10) && (y != -10)){break;}
 		}
+		//Break loop if the temp vars are filled
 		if ((x != -10) && (y != -10)){break;}
 	}
+	//Check for misses
 	if(player[x][y] == EMPTY){
 		player[x][y] = MISS;
 		std::cout << "Computer MISS!" << std::endl;
+	//Check for hits
 	} else {
 		player[x][y] = HIT;
 		std::cout << "Computer HIT!" << std::endl;
